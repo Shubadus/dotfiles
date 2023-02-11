4d9543f44c4890a1ea20ef21b98b74dc5d0383e5
Cody Shumer <cshumer@localhost.localdomain>
2023-02-11 10:22:38 -0500

    updated qtile config

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -11,14 +11,14 @@ function run {
 autorandr -c
 
 # Run fehbg to restore background image
-# run $HOME/.fehbg
+run $HOME/.fehbg
 
 #starting utility applications at boot time
 run nm-applet # Run Network Manager applet
 run xfce4-power-manager # Run Power Manager
-run variety
-run remmina -i
-run 1password --silent
+# run variety
+# run remmina -i
+# run 1password --silent
 run picom --config $HOME/.config/qtile/scripts/picom.conf # Run compositor
 run /usr/libexec/polkit-gnome-authentication-agent-1 # Polkit for authentication
 run /usr/bin/dunst -conf $HOME/.config/dunst/dunstrc # Notifications
