63276467f75f1fc26558098bbf267ed945474c73
Cody Shumer <crshumer@gmail.com>
2024-07-15 15:55:42 -0400

    Update .local/bin/scripts/hyprland-gtk-theme.sh

--- private_dot_local/bin/scripts/executable_hyprland-gtk-theme.sh
+++ private_dot_local/bin/scripts/executable_hyprland-gtk-theme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 active_border=$(grep -oP -m 1 'accent_bg_color \K.*\)' .config/gtk-3.0/gtk.css)
 inactive_border=$(grep -oP -m 1 'view_bg_color \K.*\)' .config/gtk-3.0/gtk.css)
-hyprctl keyword general:col.active_border $active_border $active_border 45deg
-hyprctl keyword general:col.inactive_border $inactive_border $inactive_border 45deg
+hyprctl keyword general:col.active_border $active_border # $active_border 45deg
+hyprctl keyword general:col.inactive_border $inactive_border # $inactive_border 45deg
