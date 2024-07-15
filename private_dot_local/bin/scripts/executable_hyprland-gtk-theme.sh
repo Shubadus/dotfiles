be388f8461b01d1089cf7e5c27c3f42d6f3d1b27
Cody Shumer <crshumer@gmail.com>
2024-07-15 16:02:38 -0400

    Update .config/hypr/hyprland.conf Update .local/bin/scripts/hyprland-gtk-theme.sh

--- private_dot_local/bin/scripts/executable_hyprland-gtk-theme.sh
+++ private_dot_local/bin/scripts/executable_hyprland-gtk-theme.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 active_border=$(grep -oP -m 1 'accent_bg_color \K.*\)' .config/gtk-3.0/gtk.css)
+active_border_rgb_values=$(echo "$active_border" | sed -E 's/rgba\(([0-9]+), ([0-9]+), ([0-9]+), [0-9.]+\)/\1 \2 \3/')
+active_border_hex=$(printf "rgba(%02x%02x%02xaa)" $active_border_rgb_values)
 inactive_border=$(grep -oP -m 1 'view_bg_color \K.*\)' .config/gtk-3.0/gtk.css)
-hyprctl keyword general:col.active_border $active_border # $active_border 45deg
-hyprctl keyword general:col.inactive_border $inactive_border # $inactive_border 45deg
+inactive_border_rgb_values=$(echo "$inactive_border" | sed -E 's/rgba\(([0-9]+), ([0-9]+), ([0-9]+), [0-9.]+\)/\1 \2 \3/')
+inactive_border_hex=$(printf "rgba(%02x%02x%02xaa)" $inactive_border_rgb_values)
+hyprctl keyword general:col.active_border $active_border_hex # $active_border 45deg
+hyprctl keyword general:col.inactive_border $inactive_border_hex # $inactive_border 45deg
