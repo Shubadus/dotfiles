d15fc2f5826feab146f95a1e2cd73550d28a9be4
Cody Shumer <crshumer@gmail.com>
2024-07-17 09:34:32 -0400

    Update .config/hypr/hyprland.conf Update .config/swaync/config.json Update .local/bin/scripts/hyprland-gtk-theme.sh

--- private_dot_local/bin/scripts/executable_hyprland-gtk-theme.sh
+++ private_dot_local/bin/scripts/executable_hyprland-gtk-theme.sh
@@ -5,5 +5,5 @@ active_border_hex=$(printf "rgba(%02x%02x%02xaa)" $active_border_rgb_values)
 inactive_border=$(grep -oP -m 1 'view_bg_color \K.*\)' .config/gtk-3.0/gtk.css)
 inactive_border_rgb_values=$(echo "$inactive_border" | sed -E 's/rgba\(([0-9]+), ([0-9]+), ([0-9]+), [0-9.]+\)/\1 \2 \3/')
 inactive_border_hex=$(printf "rgba(%02x%02x%02xaa)" $inactive_border_rgb_values)
-hyprctl keyword general:col.active_border $active_border_hex # $active_border 45deg
-hyprctl keyword general:col.inactive_border $inactive_border_hex # $inactive_border 45deg
+hyprctl keyword general:col.active_border $active_border_hex
+hyprctl keyword general:col.inactive_border $inactive_border_hex
