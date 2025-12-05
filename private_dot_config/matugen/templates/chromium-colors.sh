e6b34c67943ffdd604d29e4802a780d16168f10b
Shubadus <crshumer@gmail.com>
2025-12-05 14:32:32 -0500

    Update .config/matugen/config.toml Add .config/matugen/templates/btop.theme Update .config/matugen/templates/chromium-colors.sh Add .config/matugen/templates/colors.css Add .config/matugen/templates/dunstrc Add .config/matugen/templates/foot.ini Update .config/matugen/templates/fuzzel.ini Update .config/matugen/templates/gtk-colors.css Add .config/matugen/templates/hyprlock.conf Add .config/matugen/templates/ly-dm.ini Add .config/matugen/templates/matugen.kvconfig Update .config/matugen/templates/niri-colors.kdl Update .config/matugen/templates/qtct-colors.conf Update .config/matugen/templates/starship-colors.toml Update .config/matugen/templates/steam.css Add .config/matugen/templates/waybar/clock.jsonc Add .config/matugen/templates/waybar/memory.jsonc Add .config/matugen/templates/waybar/mpris.jsonc Add .config/matugen/templates/waybar/notification.jsonc Add .config/matugen/templates/waybar/updates.jsonc Add .config/matugen/templates/waybar/weather.jsonc

--- private_dot_config/matugen/templates/chromium-colors.sh
+++ private_dot_config/matugen/templates/chromium-colors.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 color="{{ colors.surface_container_low.default.red }}, {{ colors.surface_container_low.default.green}}, {{ colors.surface_container_low.default.blue}}"
+chromium --no-startup-window --set-theme-color="$color" >/dev/null 2>&1
