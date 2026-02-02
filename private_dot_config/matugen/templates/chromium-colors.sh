6958aa10cbfd4539762a8879139bffe4da291631
Shubadus <crshumer@gmail.com>
2026-02-02 16:30:20 -0500

    Update .config/btop/btop.conf Update .config/matugen/config.toml Update .config/matugen/templates/chromium-colors.sh Update .config/matugen/templates/fuzzel.ini Update .config/niri/binds.kdl Update .config/niri/color.kdl Update .config/niri/config.kdl Add .config/niri/config.kdl.backup Add .config/niri/config.kdl.backup1768431980 Update .config/niri/dms/alttab.kdl Update .config/niri/dms/binds.kdl Update .config/niri/dms/colors.kdl Update .config/niri/dms/config.kdl Add .config/niri/dms/cursor.kdl Update .config/niri/dms/layout.kdl Add .config/niri/dms/outputs.kdl Update .config/niri/dms/wpblur.kdl Add .config/niri/exo.kdl Update .config/niri/noctalia.kdl Update .config/niri/rules.kdl Update .config/niri/waybar/waybar.kdl Update .config/nvim/lazy-lock.json

--- private_dot_config/matugen/templates/chromium-colors.sh
+++ private_dot_config/matugen/templates/chromium-colors.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-color="{{ colors.surface_container_low.default.red }}, {{ colors.surface_container_low.default.green}}, {{ colors.surface_container_low.default.blue}}"
+color="{{ colors.surface.default.red }}, {{ colors.surface.default.green}}, {{ colors.surface.default.blue}}"
 chromium --no-startup-window --set-theme-color="$color" >/dev/null 2>&1
